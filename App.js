32179285bb65f71c7610bcb3b94051fb8333be36
Lujie Ma <xiaolinzzz2002@gmail.com>
2023-09-06 19:19:17 +1000

    ignore

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App () {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 12,
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
