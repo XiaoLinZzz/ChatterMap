a5457d841d30a0dfffc5d27824287650663eff78
Bob <zhouyu@kean.edu>
2023-10-15 17:43:51 +1100

    Totally successful login and signup page

--- pages/Register.js
+++ pages/Register.js
@@ -4,23 +4,42 @@ import { List, TextInput } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function SignupPage() {
-  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const handleSignUp = async () => {
     setLoading(true);
     setError(null);
+    setIsSuccess(false);
     const data = {
-      username: username,
+      name: name,
       email: email,
       password: password
     };
     try {
-      const response = await postData('http://localhost:9999', data);
-      console.log(response);  // Handle the response as required
+      const response = await fetch('http://3.138.178.239:5000/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        throw new Error(responseData.message || 'Sign Up Failed.');
+      }
+
+      if (responseData.id && responseData.name && responseData.email) {
+        setIsSuccess(true);
+      } else {
+        throw new Error('Unexpected response from the server.');
+      }
     } catch (error) {
       setError(error.message);
     } finally {
@@ -31,29 +50,27 @@ export default function SignupPage() {
   return (
     <SafeAreaView style={styles.screen}>
       <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.logoContainer}>
-          {/* This is a placeholder for your logo */}
-          <Text style={styles.logo}>Sign-up Page</Text>
-        </View>
+        <Text style={styles.pageTitle}>Create your account</Text>
+        {isSuccess && <Text style={styles.successText}>Account created successfully!</Text>}
         <List.Section style={styles.inputSection}>
           <TextInput
-            label="Username"
-            value={username}
-            onChangeText={setUsername}
-            left={<TextInput.Icon name={() => <Icon name="user" />} />}
+            label="Name"
+            value={name}
+            onChangeText={setName}
+            left={<TextInput.Icon name="user" icon={() => <Icon name="user" size={20} />} />}
           />
           <TextInput
             label="Email"
             value={email}
             onChangeText={setEmail}
-            left={<TextInput.Icon name={() => <Icon name="envelope" />} />}
+            left={<TextInput.Icon name="envelope" icon={() => <Icon name="envelope" size={18} />} />}
           />
           <TextInput
             label="Password"
             value={password}
             secureTextEntry={true}
             onChangeText={setPassword}
-            left={<TextInput.Icon name={() => <Icon name="lock" />} />}
+            left={<TextInput.Icon name="lock" icon={() => <Icon name="lock" size={20} />} />}
           />
           {error && <Text style={styles.errorText}>{error}</Text>}
           <TouchableOpacity style={styles.signupButton} onPress={handleSignUp} disabled={loading}>
@@ -69,6 +86,15 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     marginHorizontal: 20,
+  },
+  pageTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  container: {
+    margin: 30,
   },
   signupButton: {
     backgroundColor: '#6200ee',
@@ -87,5 +113,11 @@ const styles = StyleSheet.create({
     color: 'red',
     marginTop: 10,
     textAlign: 'center',
+  },
+  successText: {
+    color: 'green',
+    fontSize: 18,
+    textAlign: 'center',
+    marginVertical: 15,
   }
 });
