f54114ff6e446c3d4c353a86a35b9d113dd5734d
Dreamer-T <1727648092@qq.com>
2023-10-22 23:50:42 +1100

    fundamental group chatscreen established

--- pages/ChatScreen.js
+++ pages/ChatScreen.js
@@ -1,51 +1,132 @@
-import React, { useState } from 'react'
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
-// import Slider from '@react-native-community/slider';
-import { useNavigation } from '@react-navigation/native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { getGroupChatInformation } from '../Services/GroupChatService';
 
-export default function GroupChatListScreen () {
-  const navigation = useNavigation()
-  const [topPadding, setTopPadding] = useState(0)
-  const [groupChats, setGroupChats] = useState([
-    { id: '1', name: 'Family Chat', members: ['Alice', 'Bob', 'Charlie'] },
-    { id: '2', name: 'Friends Chat', members: ['David', 'Eve', 'Frank'] },
-    { id: '3', name: 'Work Chat', members: ['Grace', 'Hank', 'Ivy'] }
-    // Add more group chats here
-  ])
+const Stack = createNativeStackNavigator();
 
+
+const chatRooms = [
+  { id: 1, name: "Chat Room 1", description: "sfsdf" },
+  { id: 2, name: "Chat Room 2", description: "sfasdfasdfasdfsadfsadsdf" },
+  // Add more chat rooms as needed
+];
+
+function MainChatScreen() {
+  const navigation = useNavigation();
+  const [name, setName] = useState("");
+  const [chatroomData, setData] = useState()
+  useEffect(() => {
+    async function test() {
+      try {
+        const data = await getGroupChatInformation(1);
+        setData(data)
+        // setName(data.name);
+        // console.log(data)
+      } catch (error) {
+        console.error("Failed to fetch user data:", error);
+      }
+    }
+
+    test();
+  }, []);
+
+  const joinChatRoom = (chatRoomId) => {
+    navigation.navigate('ChatRoom', { chatRoomId });
+  };
   const renderGroupChatItem = ({ item }) => (
     <TouchableOpacity
       style={styles.groupChatItem}
-      onPress={() => handleGroupChatPress(item)}
+      onPress={() => joinChatRoom(item)}
     >
       <Text style={styles.groupChatName}>{item.name}</Text>
-      <Text style={styles.groupChatMembers}>
-        {item.members.join(', ')}
-      </Text>
+      <Text style={styles.groupChatMembers}>{item.description}</Text>
     </TouchableOpacity>
   )
-
-  const handleGroupChatPress = (groupChat) => {
-    // Handle group chat item press, e.g., navigate to the chat screen
-    console.log(`Pressed group chat: ${groupChat.name}`)
-    // navigation.navigate('GroupChat', { groupChat });
-  }
-
   return (
-  <SafeAreaView style={{ flex: 1, paddingTop: topPadding }}>
-    <View style={styles.header}>
-      <Text style={styles.headerTitle}>Group Chats</Text>
+    <View>
+      <FlatList
+        data={[chatroomData]}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderGroupChatItem}
+      />
     </View>
-    <FlatList
-      data={groupChats}
-      keyExtractor={(item) => item.id}
-      renderItem={renderGroupChatItem}
-    />
-  </SafeAreaView>
-  )
+  );
 }
 
+function ChatRoomScreen({ route }) {
+  const navigation = useNavigation();
+  const { chatRoomId } = route.params;
+
+  const [messages, setMessages] = React.useState([]);
+  const [newMessage, setNewMessage] = React.useState('');
+  const addMessage = (text, sender) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { id: prevMessages.length, text, sender },
+    ]);
+  };
+
+  // change the title of the chat room
+  React.useEffect(() => {
+    // 名字有问题
+    navigation.setOptions({ title: `Chat Room ${chatRoomId}` });
+  }, [chatRoomId]);
+
+
+  const sendMessage = () => {
+    if (newMessage) {
+      addMessage(newMessage, 'user');
+      setNewMessage('');
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, padding: 10 }}>
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              alignSelf: item.sender === 'user' ? 'flex-end' : 'flex-start',
+              backgroundColor: item.sender === 'user' ? '#007AFF' : '#E5E5EA',
+              borderRadius: 10,
+              margin: 5,
+              maxWidth: '70%',
+              padding: 10,
+            }}
+          >
+            <Text style={{ color: item.sender === 'user' ? 'white' : 'black' }}>
+              {item.text}
+            </Text>
+          </View>
+        )}
+      />
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TextInput
+          style={{ flex: 1, borderWidth: 1, marginRight: 5, padding: 10, borderRadius: 10 }}
+          value={newMessage}
+          onChangeText={setNewMessage}
+          multiline={true}
+        />
+        <Button title="send" onPress={sendMessage} />
+      </View>
+    </View>
+  );
+}
+
+function ChatScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={MainChatScreen} />
+      <Stack.Screen name="ChatRoom" component={ChatRoomScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default ChatScreen;
 const styles = StyleSheet.create({
   groupChatItem: {
     paddingHorizontal: 16,
@@ -60,14 +141,6 @@ const styles = StyleSheet.create({
   groupChatMembers: {
     fontSize: 14,
     color: '#888'
-  },
-  header: {
-    flexDirection: 'row',
-    backgroundColor: 'tomato',
-    padding: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-    alignItems: 'center'
   },
   headerTitle: {
     fontSize: 24,
--- pages/Chatroom/Chatroom.js
+++ pages/Chatroom/Chatroom.js
@@ -1,79 +1,15 @@
-import React, { useState } from 'react'
-import { View, Text, TextInput, TouchableOpacity, FlatList, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-const Chatroom = () => {
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([])
-
-  const handleSend = () => {
-    if (message.trim() === '') return
-
-    setMessages([...messages, { text: message, user: 'me' }])
-    setMessage('')
-  }
+export default function Chatroom() {
+  const route = useRoute();
+  const groupChat = route.params.groupChat;
 
   return (
-        <View style={styles.container}>
-            <FlatList
-                data={messages}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
-                    <View style={item.user === 'me' ? styles.myMessage : styles.otherMessage}>
-                        <Text>{item.text}</Text>
-                    </View>
-                )}
-            />
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Type your message..."
-                    value={message}
-                    onChangeText={(text) => setMessage(text)}
-                />
-                <TouchableOpacity style={styles.sendButton} onPress={handleSend}>
-                    <Text>Send</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-  )
+    <View>
+      <Text>Chat Room: {groupChat.name}</Text>
+      {/* 在这里显示聊天室的内容 */}
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16
-  },
-  myMessage: {
-    alignSelf: 'flex-end',
-    backgroundColor: 'lightblue',
-    padding: 8,
-    marginVertical: 4,
-    borderRadius: 8
-  },
-  otherMessage: {
-    alignSelf: 'flex-start',
-    backgroundColor: 'lightgray',
-    padding: 8,
-    marginVertical: 4,
-    borderRadius: 8
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  input: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    padding: 8,
-    marginRight: 8
-  },
-  sendButton: {
-    backgroundColor: 'tomato',
-    padding: 8,
-    borderRadius: 8
-  }
-})
-
-export default Chatroom
