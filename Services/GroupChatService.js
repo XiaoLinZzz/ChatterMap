73c0f360c74b8a1f94dc59ad022f7d1ad3a77908
Dreamer-T <1727648092@qq.com>
2023-10-23 00:56:25 +1100

    new chatroom screen with send message function locally

---
+++ Services/GroupChatService.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+let BASIC_URL = "http://3.138.178.239:5000/groupchats/1";
+
+export async function getGroupChatInformation(groupId) {
+    const response = await fetch(`${BASIC_URL}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${await AsyncStorage.getItem('userToken')}`
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // console.log(typeof data)
+    // console.log(data);
+    return data;
+}
