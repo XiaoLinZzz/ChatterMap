735c2ba7ebe8ac0865a4aa1b8accef234a6b8abc
Dreamer-T <1727648092@qq.com>
2023-10-24 16:37:34 +1100

    restructure the chat screen file

--- pages/Profile/Setting.js
+++ pages/Profile/Setting.js
@@ -22,7 +22,7 @@ export default function SettingScreen() {
 
     fetchAndSetVibrationState();
   }, []);
-  
+
   const switchNotification = () => {
     setNotificationEnabled((previousState) => !previousState)
   }
