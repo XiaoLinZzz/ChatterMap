9c927fef49c285e1db9db1a169d5d125165ab312
Seulttom <734697407@qq.com>
2023-11-08 03:43:46 +1100

    modified:   pages/Chatroom/ChatScreen.js 	modified:   pages/Chatroom/Chatroom.js

--- pages/Chatroom/Chatroom.js
+++ pages/Chatroom/Chatroom.js
@@ -1,8 +1,13 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { Button, FlatList, Text, TextInput, View } from 'react-native';
-import { sendGroupMessage, getLastNMessageInformation } from '../../Services/GroupChatService';
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import React, { useEffect, useState } from 'react'; 
+import { Button, FlatList, Text, TextInput, View, KeyboardAvoidingView, Platform, SafeAreaView, Keyboard } from 'react-native'; 
+import { useNavigation } from '@react-navigation/native'; 
+import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import { sendGroupMessage, getLastNMessageInformation } from '../../Services/GroupChatService'; 
+import { useHideTab } from '../../HideTabContext';
+import { useFocusEffect } from '@react-navigation/native';
+
+
+
 
 export function ChatRoomScreen({ route }) {
   const navigation = useNavigation();
@@ -15,6 +20,18 @@ export function ChatRoomScreen({ route }) {
   const [scrollY, setScrollY] = useState(0);
   const [top, setTop] = useState(true)
   const [bottom, setBottom] = useState(true)
+  const { hideTab, setHideTab } = useHideTab();
+
+  const flatListRef = React.useRef(null);
+
+  useFocusEffect( 
+    React.useCallback(() => { 
+      //setHideTab('none');
+      //setNewMessage('');
+      return () => setHideTab('flex'); 
+    }, [setHideTab]) 
+  );
+
 
   const addMessage = async (content) => {
     const user_id = await AsyncStorage.getItem('userId')
@@ -33,6 +50,10 @@ export function ChatRoomScreen({ route }) {
   const handleScroll = (event) => {
     const currentY = event.nativeEvent.contentOffset.y;
 
+    // const threshold = 50;
+    // if(currentY < threshold){
+    //   loadMoreHistoryMessages();
+    // }
     // up or down
     if (currentY <= 0) {
       // up
@@ -45,13 +66,40 @@ export function ChatRoomScreen({ route }) {
 
     setScrollY(currentY);
   };
+
+  useEffect(() => { 
+    const scrollToBottom = () => { 
+      flatListRef.current?.scrollToEnd({ animated: false }); 
+    }; 
+    
+    // 加载消息后滚动到底部 
+    const unsubscribeFocus = navigation.addListener('focus', scrollToBottom); 
+    
+    // 键盘显示时滚动到底部 
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', scrollToBottom); 
+    
+    // 返回时移除监听 
+    return () => { unsubscribeFocus(); keyboardDidShowListener.remove(); 
+    }; 
+  }, [navigation]); 
+  
+  // 发送消息时滚动到底部 
+  useEffect(() => { 
+    if (messages.length > 0) { 
+      flatListRef.current?.scrollToEnd({ animated: true }); 
+    } 
+  }, [messages]);
+
+
   // change the title of the chat room
   useEffect(() => {
+    
     navigation.setOptions({ title: `${chatRoomId.name}` });
+    
     async function getMessages() {
       const messagesInChat = await getLastNMessageInformation(chatRoomId.id)
       // console.log(messagesInChat)
-      addMessages(messagesInChat)
+      addMessages(messagesInChat);
     }
     getMessages()
   }, [chatRoomId]);
@@ -66,7 +114,6 @@ export function ChatRoomScreen({ route }) {
       }))
     ]);
   };
-
 
   const sendMessage = () => {
     if (newMessage) {
@@ -112,41 +159,54 @@ export function ChatRoomScreen({ route }) {
   };
 
   return (
-    <View style={{ flex: 1, padding: 10 }}>
-      <FlatList
-        data={messages}
-        keyExtractor={(item) => item.id ? item.id.toString() : Math.random().toString()}
-        refreshing={isRefreshing}
-        onRefresh={onRefresh}
-        onScroll={handleScroll}
-        onEndReached={loadMoreHistoryMessages}
-        onEndReachedThreshold={0.01}
-        renderItem={({ item }) => (
-          <View
-            style={{
-              alignSelf: item.sender === 'user' ? 'flex-end' : 'flex-start',
-              backgroundColor: item.sender === 'user' ? '#007AFF' : '#E5E5EA',
-              borderRadius: 10,
-              margin: 5,
-              maxWidth: '70%',
-              padding: 10,
-            }}
-          >
-            <Text style={{ color: item.sender === 'user' ? 'white' : 'black' }}>
-              {item.text}
-            </Text>
-          </View>
-        )}
-      />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <TextInput
-          style={{ flex: 1, borderWidth: 1, marginRight: 5, padding: 10, borderRadius: 10 }}
-          value={newMessage}
-          onChangeText={setNewMessage}
-          multiline={true}
+    <SafeAreaView style={{ flex: 1}}>
+      <KeyboardAvoidingView
+        style = {{flex: 1}}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 85:0}
+      >
+        <View style={{ flex: 1, padding: 10 }}>
+          <FlatList
+            ref={flatListRef}
+            data={messages}
+            keyExtractor={(item) => item.id ? item.id.toString() : Math.random().toString()}
+            refreshing={isRefreshing}
+            onRefresh={onRefresh}
+            onScroll={handleScroll}
+            onEndReached={loadMoreHistoryMessages}
+            onEndReachedThreshold={0.01}
+            renderItem={({ item }) => (
+              <View
+                style={{
+                alignSelf: item.sender === 'user' ? 'flex-end' : 'flex-start',
+                backgroundColor: item.sender === 'user' ? '#007AFF' : '#E5E5EA',
+                borderRadius: 10,
+                margin: 5,
+                maxWidth: '70%',
+                padding: 10,
+                }}
+              >
+              <Text style={{ color: item.sender === 'user' ? 'white' : 'black' }}>
+                {item.text}
+              </Text>
+            </View>
+          )}
         />
-        <Button title="Send" onPress={sendMessage} />
+        <View style={{ 
+          flexDirection: 'row', 
+          alignItems: 'center',
+          paddingBottom: 0
+          }}>
+          <TextInput
+            style={{ flex: 1, borderWidth: 1, marginRight: 5, padding: 10, borderRadius: 10 }}
+            value={newMessage}
+            onChangeText={setNewMessage}
+            multiline={true}
+          />
+          <Button title="Send" onPress={sendMessage} />
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
